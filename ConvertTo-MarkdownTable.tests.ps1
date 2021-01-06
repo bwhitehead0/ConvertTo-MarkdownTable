c4de97acc2073e9aec2005b5b75243e83dd44972
whitehead.brandon@gmail.com <whitehead.brandon@gmail.com>
2021-01-06 00:36:31 -0600

    added basic initial tests.

---
+++ ConvertTo-MarkdownTable.tests.ps1
@@ -0,0 +1,34 @@
+BeforeAll {
+    # load the script
+    . ./ConvertTo-MarkdownTable.ps1
+}
+
+Describe "ConvertTo-MarkdownTable" {
+
+    Context "-InputObject provided Only" {
+        It "returns string array" {
+            $customObject = [PSCustomObject]@{a=1;c=3;b=2;d=4}
+            $result = ConvertTo-MarkdownTable -InputObject $customObject
+            $result.count | Should -BeGreaterOrEqual 1
+        }
+    }
+    Context "-Jira provided" {
+        It "uses '||' as header separator" {
+            $customObject = [PSCustomObject]@{a=1;c=3;b=2;d=4}
+            $result = ConvertTo-MarkdownTable -InputObject $customObject -Jira
+            $result[0] | Should -BeLike "*||*"
+        }
+    }
+    Context "-ReplaceNewline provided" {
+        It "replaces value CRLF with HTML <br>" {
+            $customObject = [PSCustomObject]@{
+                a="multi`r`nline`r`ntext"
+                c=3
+                b=2
+                d=4
+            }
+            $result = ConvertTo-MarkdownTable -InputObject $customObject -ReplaceNewline
+            $result[2] | Should -BeLike "*<br>*"
+        }
+    }
+}
